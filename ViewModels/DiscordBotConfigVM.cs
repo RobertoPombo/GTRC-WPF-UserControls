0bacecfffcfc31fbc10d7286b41013c91cb90a96
RobertoPombo <captn.sponge2@web.de>
2024-02-25 03:26:02 +0100

    Added DiscordBotConfigVM.Series, .Season.

--- ViewModels/DiscordBotConfigVM.cs
+++ ViewModels/DiscordBotConfigVM.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections.ObjectModel;
+using System.Net;
 
 using GTRC_Basics;
 using GTRC_Basics.Configs;
+using GTRC_Basics.Models;
+using GTRC_Database_Client.Responses;
+using GTRC_Database_Client;
 using GTRC_WPF;
 
 namespace GTRC_WPF_UserControls.ViewModels
@@ -9,6 +13,8 @@ namespace GTRC_WPF_UserControls.ViewModels
     public class DiscordBotConfigVM : ObservableObject
     {
         private DiscordBotConfig? selected;
+        private Series? series;
+        private Season? season;
 
         public DiscordBotConfigVM()
         {
@@ -28,7 +34,40 @@ namespace GTRC_WPF_UserControls.ViewModels
         public DiscordBotConfig? Selected
         {
             get { return selected; }
-            set { selected = value; RaisePropertyChanged(); RaisePropertyChanged(nameof(IsActive)); }
+            set { selected = value; RaisePropertyChanged(); RaisePropertyChanged(nameof(IsActive)); _ = GetListSeries(); }
+        }
+
+        public ObservableCollection<Series> ListSeries { get; set; } = [];
+
+        public ObservableCollection<Season> ListSeasons { get; set; } = [];
+
+        public Series? Series
+        {
+            get { return series; }
+            set
+            {
+                if (series?.Id != value?.Id)
+                {
+                    series = value;
+                    if (selected is not null && series is not null && selected.SeriesId != series.Id) { selected.SeriesId = series.Id; }
+                    _ = GetListSeasons();
+                }
+                RaisePropertyChanged();
+            }
+        }
+
+        public Season? Season
+        {
+            get { return season; }
+            set
+            {
+                if (season?.Id != value?.Id)
+                {
+                    season = value;
+                    if (selected is not null && season is not null && selected.SeasonId != season.Id) { selected.SeasonId = season.Id; }
+                }
+                RaisePropertyChanged();
+            }
         }
 
         public bool IsActive
@@ -47,11 +86,42 @@ namespace GTRC_WPF_UserControls.ViewModels
 
         public DiscordBotConfig? ConfirmActiveBot()
         {
-            OnConfirmDiscordBotActivated();
+            OnChangedDiscordBotIsActive();
             DiscordBotConfig? activeBot = DiscordBotConfig.GetActiveBot();
             if (activeBot is null) { GlobalValues.CurrentLogText = "Discord bot stopped."; }
             else { GlobalValues.CurrentLogText = "Discord bot is running."; }
             return activeBot;
+        }
+
+        public async Task GetListSeries()
+        {
+            Series = null;
+            ListSeries = [];
+            DbApiListResponse<Series> response = await DbApi.DynConnection.Series.GetAll();
+            if (response.Status == HttpStatusCode.OK)
+            {
+                foreach (Series obj in response.List) { ListSeries.Add(obj); }
+            }
+            RaisePropertyChanged(nameof(ListSeries));
+            foreach (Series obj in ListSeries) { if (obj.Id == selected?.SeriesId) { Series = obj; } }
+            if (Series is null && ListSeries.Count > 0) { Series = ListSeries[0]; }
+        }
+
+        public async Task GetListSeasons()
+        {
+            Season = null;
+            ListSeasons = [];
+            if (Series is not null)
+            {
+                DbApiListResponse<Season> response = await DbApi.DynConnection.Season.GetChildObjects(typeof(Series), Series.Id);
+                if (response.Status == HttpStatusCode.OK)
+                {
+                    foreach (Season obj in response.List) { ListSeasons.Add(obj); }
+                }
+            }
+            RaisePropertyChanged(nameof(ListSeasons));
+            foreach (Season obj in ListSeasons) { if (obj.Id == selected?.SeasonId) { Season = obj; } }
+            if (Season is null && ListSeasons.Count > 0) { Season = ListSeasons[0]; }
         }
 
         public void RestoreJson()
@@ -79,9 +149,9 @@ namespace GTRC_WPF_UserControls.ViewModels
             }
         }
 
-        public static event Notify? ConfirmDiscordBotActivated;
+        public static event Notify? ChangedDiscordBotIsActive;
 
-        public static void OnConfirmDiscordBotActivated() { ConfirmDiscordBotActivated?.Invoke(); }
+        public static void OnChangedDiscordBotIsActive() { ChangedDiscordBotIsActive?.Invoke(); }
 
         public UICmd RestoreJsonCmd { get; set; }
         public UICmd SaveJsonCmd { get; set; }
