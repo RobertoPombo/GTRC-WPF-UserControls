8c24b87e4e0ecdb550949876a548b16d9ff13de8
RobertoPombo <captn.sponge2@web.de>
2024-07-31 19:58:20 +0200

    Added DiscordBot, Discord Commands.

---
+++ Scripts/DiscordBot.cs
@@ -0,0 +1,148 @@
+﻿using Discord.Commands;
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+using GTRC_Basics.Configs;
+using GTRC_WPF_UserControls.ViewModels;
+using GTRC_Basics;
+
+namespace GTRC_WPF_UserControls.Scripts
+{
+    public class DiscordBot
+    {
+        private CommandService commands = new();
+        private InteractionService? interactions;
+        private IServiceProvider? services;
+        private DiscordSocketConfig socketConfig = new() { AlwaysDownloadUsers = false, GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent };
+
+        public DiscordSocketClient? Client;
+        public DiscordBotConfig? Config;
+
+        public DiscordBot()
+        {
+            Initialize();
+            DiscordBotConfigVM.ChangedDiscordBotIsActive += Initialize;
+        }
+
+        private Task ClientLog(LogMessage arg) { return Task.CompletedTask; }
+
+        public void EnsureIsRunning()
+        {
+            if (Client is not null) { GlobalValues.CurrentLogText = "Discord bot is running."; }
+            else { GlobalValues.CurrentLogText = "Discord bot stopped."; }
+        }
+
+        private async Task RegisterCommandsAsync()
+        {
+            if (Client is not null)
+            {
+                Client.MessageReceived += HandleCommandAsync;
+                await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+            }
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            if (Config is not null && Client is not null)
+            {
+                SocketUserMessage? userMessage = arg as SocketUserMessage;
+                SocketCommandContext context = new(Client, userMessage);
+                int argPos = 0;
+                List<string> Tags = [];
+                List<string> TagList = GetTagsByDiscordId(Config.DiscordBotId);
+                foreach (string _tag in TagList) { Tags.Add(_tag + " "); }
+                Tags.Add("!");
+                foreach (string tag in Tags)
+                {
+                    if (userMessage?.HasStringPrefix(tag, ref argPos) ?? false)
+                    {
+                        var result = await commands.ExecuteAsync(context, argPos, services);
+                        if (!result.IsSuccess && userMessage is not null)
+                        {
+                            await userMessage.DeleteAsync();
+                        }
+                    }
+                }
+            }
+        }
+
+        public async void Initialize()
+        {
+            Client?.Dispose();
+            Config = DiscordBotConfig.GetActiveBot();
+            if (Config is not null)
+            {
+                Client = new(socketConfig);
+                services = new ServiceCollection().AddSingleton(Client).AddSingleton(commands).BuildServiceProvider();
+                Client.Log += ClientLog;
+                await RegisterCommandsAsync();
+                await Client.LoginAsync(TokenType.Bot, Config.Token);
+                await Client.StartAsync();
+            }
+        }
+
+        public async Task SendMessage(string messageContent, ulong channelId)
+        {
+            if (Config is not null && Client is not null)
+            {
+                SocketTextChannel? channel = Client.GetGuild(Config.DiscordServerId)?.GetTextChannel(channelId);
+                if (channel is not null) { await SendMessageRecursive(channel, messageContent); }
+            }
+        }
+
+        public async Task SendMessageRecursive(SocketTextChannel channel, string messageContent)
+        {
+            List<string> keys = ["**\n", "\n"];
+            string part1;
+            string part2;
+            if (Config is not null && messageContent.Length > Config.CharLimit)
+            {
+                int keyNr = 0;
+                string key = keys[keyNr];
+                int charPos = Config.CharLimit;
+                while (true)
+                {
+                    charPos--;
+                    if (charPos == key.Length && keyNr < keys.Count - 1)
+                    {
+                        keyNr++;
+                        key = keys[keyNr];
+                        charPos = Config.CharLimit;
+                    }
+                    else if (charPos == key.Length)
+                    {
+                        part1 = messageContent[..Config.CharLimit];
+                        part2 = messageContent[Config.CharLimit..];
+                        break;
+                    }
+                    else if (messageContent.Substring(charPos - key.Length, key.Length) == key)
+                    {
+                        part1 = messageContent[..charPos];
+                        part2 = messageContent[charPos..];
+                        break;
+                    }
+                }
+                await SendMessage(channel, part1);
+                await SendMessageRecursive(channel, part2);
+            }
+            else
+            {
+                await SendMessage(channel, messageContent);
+            }
+        }
+
+        public static async Task SendMessage(SocketTextChannel channel, string MessageContent)
+        {
+            await channel.SendMessageAsync(MessageContent);
+        }
+
+        public static List<string> GetTagsByDiscordId(ulong discordId)
+        {
+            List<string> TagList = ["<@" + discordId.ToString() + ">", "<@!" + discordId.ToString() + ">"];
+            return TagList;
+        }
+    }
+}
---
+++ Scripts/DiscordCommandsBase.cs
@@ -0,0 +1,99 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Net;
+
+using GTRC_Basics;
+using GTRC_Basics.Configs;
+using GTRC_Basics.Models;
+using GTRC_Basics.Models.DTOs;
+using GTRC_Database_Client;
+using GTRC_Database_Client.Responses;
+using GTRC_WPF_UserControls.ViewModels;
+
+namespace GTRC_WPF_UserControls.Scripts
+{
+    public class DiscordCommandsBase : ModuleBase<SocketCommandContext>
+    {
+        public static DiscordCommandsBase? Instance;
+        public static readonly DiscordBot DiscordBot = new();
+
+        public static readonly Emoji emojiSuccess = new("✅");
+        public static readonly Emoji emojiFail = new("❌");
+        public static readonly Emoji emojiWTF = new("🤷‍♀️");
+        public static readonly Emoji emojiSleep = new("😴");
+        public static readonly Emoji emojiShocked = new("😱");
+        public static readonly Emoji emojiRaceCar = new("🏎️");
+        public static readonly Emoji emojiPartyFace = new("🥳");
+        public static readonly Emoji emojiCry = new("😭");
+        public static readonly Emoji emojiThinking = new("🤔");
+
+        public DiscordBotConfig? Config;
+        public bool IsError = false;
+        public string LogText = string.Empty;
+        public ulong AuthorDiscordId = GlobalValues.NoDiscordId;
+        public User? User;
+        public List<Role> AuthorRoles = [];
+        public bool UserIsAdmin = false;
+        public Season? Season;
+
+        private static readonly string AdminRoleName = "Communityleitung";
+
+        public DiscordCommandsBase()
+        {
+            Instance = this;
+            Initialize();
+            DiscordBotConfigVM.ChangedDiscordBotIsActive += Initialize;
+        }
+
+        public void Initialize()
+        {
+            Config = DiscordBotConfig.GetActiveBot();
+        }
+
+        public async Task SetDefaultProperties()
+        {
+            IsError = false;
+            AuthorDiscordId = Context.Message.Author.Id;
+            UniqPropsDto<User> uniqUserDto = new() { Index = 1, Dto = new UserUniqPropsDto1 { DiscordId = AuthorDiscordId } };
+            DbApiObjectResponse<User> userResponse = await DbApi.DynConnection.User.GetByUniqProps(uniqUserDto);
+            if (userResponse.Status == HttpStatusCode.OK)
+            {
+                User = userResponse.Object;
+                DbApiListResponse<Role> roleResponse = await DbApi.Connection.Role.GetByUser(userResponse.Object.Id);
+                if (roleResponse.Status == HttpStatusCode.OK) { AuthorRoles = roleResponse.List; }
+                foreach (Role role in AuthorRoles) { if (role.Name == AdminRoleName) { UserIsAdmin = true; break; } }
+            }
+            if (Config is not null)
+            {
+                DbApiObjectResponse<Season> seasonResponse = await DbApi.Connection.Season.GetById(Config.SeasonId);
+                if (seasonResponse.Status == HttpStatusCode.OK) { Season = seasonResponse.Object; }
+            }
+        }
+
+        public async Task ErrorResponse()
+        {
+            if (!IsError)
+            {
+                if (LogText.Length > 0) { await ReplyAsync(LogText); }
+                await Context.Message.RemoveAllReactionsAsync();
+                await Context.Message.AddReactionAsync(emojiFail);
+                LogText = string.Empty;
+                IsError = true;
+            }
+        }
+
+        public (List<dynamic>, int, ComponentBuilder) AddOption2Menu(string value, string name, List<dynamic> ListMenu, int optionCount, ComponentBuilder component, string id, string placeholder)
+        {
+            int countListMenu = ListMenu.Count;
+            optionCount++;
+            if (optionCount > 25)
+            {
+                ListMenu.Add(new SelectMenuBuilder() { CustomId = id + countListMenu.ToString(), Placeholder = placeholder });
+                optionCount = 1; countListMenu++;
+                component.WithSelectMenu(ListMenu[countListMenu - 2]);
+            }
+            ListMenu[countListMenu - 1].AddOption(value, name);
+            return (ListMenu, optionCount, component);
+        }
+    }
+}
